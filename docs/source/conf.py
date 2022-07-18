e587ebbd3b1b5f40e3380fbd4fc0e06dfb13044d
lc09c <45105147+lc09c@users.noreply.github.com>
2022-07-18 10:14:11 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,10 @@
 # Configuration file for the Sphinx documentation builder.
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath(r'..\..'))
+
+
 # -- Project information
 
 project = 'Lumache'
